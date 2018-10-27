8b4bc7c48439a286eb0f00568936d4530680ef8f
Root <root@deeplearnlb.co>
2018-10-27 09:00:19 +0000

    executable updates

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting the Eureka Server"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/eureka-registry-0.0.1-SNAPSHOT.jar
+sudo /usr/bin/java -Djava.security.egd=file:/dev/./urandom -jar /home/deployments/apps/eureka-registry-0.0.2-SNAPSHOT.jar
